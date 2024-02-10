57ce10affdeba97071908cf17c2f1e2888b6e64b
Gaben <gabrielbportela413@gmail.com>
2024-02-10 13:40:45 -0300

    tauri project created

--- app-project/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <HomeView/>
-</template>
-  
-<script setup>
-    import HomeView from './views/HomeView.vue';
-    console.log('👋 This message is being logged by "App.vue", included via Vite');
-</script>
