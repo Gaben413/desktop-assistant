0dc1de656b009e33f1eb2450979330c2bb6529a9
Gaben <gabrielbportela413@gmail.com>
2024-02-20 22:06:01 -0300

    Update WeatherComponent.vue

--- desktop-assistant/src/components/WeatherComponent.vue
+++ desktop-assistant/src/components/WeatherComponent.vue
@@ -19,53 +19,73 @@
     
 </template>
 
-
-
-<script setup>
+<script>
     import { fetch } from '@tauri-apps/api/http';
 
-    let now_date = new Date();
-    let current_day = now_date.getDate();
-    let current_month = now_date.getMonth()+1;
-    let current_year = now_date.getFullYear();
-    let month_days = new Date(current_year, current_month, 0).getDate();
+    export default {
 
-    console.log(current_year)
+        data(){
+            return{
+                season: function(){
 
-    //SEASON
-    
-    //[Meteorological Seasons, Oppositive Seasons]
-    let spring_months = [[3, 4, 5], [9, 10, 11]];
-    let summer_months = [[6, 7, 8], [12, 1, 2]];
-    let autumn_months = [[9, 10, 11], [3, 4, 5]];
-    let winter_months = [[12, 1, 2], [6, 7, 8]];
+                    let current_day = new Date().getDate()
+                    let current_month = new Date().getMonth()+1
+                    let current_year = new Date().getFullYear()
+                    let month_days = new Date(current_year, current_month, 0).getDate()
 
-    let season = function(){
-        let rotation = 1;
+                    let rotation = 1;
 
-        if (spring_months[rotation].includes(current_month)){
-            return 1;
-        } else if (summer_months[rotation].includes(current_month)){
-            return 2;
-        } else if (autumn_months[rotation].includes(current_month)){
-            return 3;
-        } else if (winter_months[rotation].includes(current_month)){
-            return 4;
-        } 
-    };
+                    let spring_months = [[3, 4, 5], [9, 10, 11]]
+                    let summer_months = [[6, 7, 8], [12, 1, 2]]
+                    let autumn_months = [[9, 10, 11], [3, 4, 5]]
+                    let winter_months = [[12, 1, 2], [6, 7, 8]]
 
-    //console.log(season())
+                    if (spring_months[rotation].includes(current_month)){
+                        return 1;
+                    } else if (summer_months[rotation].includes(current_month)){
+                        return 2;
+                    } else if (autumn_months[rotation].includes(current_month)){
+                        return 3;
+                    } else if (winter_months[rotation].includes(current_month)){
+                        return 4;
+                    } 
+                },
+                moon_phase: 1
+            }
+        },
+        async created() {
+            
+            let moon_data = await this.get_data();
 
-    //MOON
+            let result = moon_data.find(data => data.day == new Date().getDate())
 
-    async function get_data(){
-        return new Promise(async (resolve, reject)=>{
+            console.log("Let's go")
+            console.log(result)
+
+            if(result['phase'] == 'Last Quarter'){
+                this.moon_phase = 1
+            }else if(result['phase'] == 'New Moon'){
+                this.moon_phase = 2
+            }else if(result['phase'] == 'First Quarter'){
+                this.moon_phase = 3
+            }else if(result['phase'] == 'Full Moon'){
+                this.moon_phase = 4
+            }
+
+        },
+        methods: {
+            get_data: async() => {
+
+            let current_month = new Date().getMonth()+1
+            let current_year = new Date().getFullYear()
+            let month_days = new Date(current_year, current_month, 0).getDate()
+
             const response = await fetch('https://aa.usno.navy.mil/api/moon/phases/year?year=2024', {
                 method: 'GET',
                 timeout: 30,
             });
 
-            let obj_array = response['data']['phasedata'].filter(o => o.month == current_month)
+            let obj_array = response['data']['phasedata'].filter(o => o.month == current_month);
 
             console.log(obj_array)
 
@@ -95,25 +115,11 @@
 
             console.log(days_obj);
 
-            resolve(days_obj)
-        })
-        
-    }
+            return days_obj
 
-    let moon_phase = 1;
-
-    (async () => {
-        const moon_data = await get_data();
-        let today_data = moon_data.find(data => data.day == current_day);
-
-        if(today_data.phase == 'First Quarter'){
-            moon_phase = 3;
+            }
         }
-
-        console.log(moon_phase)
-    })();
-
-
+    }
 </script>
 
 <style scoped>
