2b9d7b05f0b2d5a653e5887433e8f851a9eaedb3
Gaben <gabrielbportela413@gmail.com>
2024-02-28 16:50:48 -0300

    System Data Integrated

--- desktop-assistant/src-tauri/src/main.rs
+++ desktop-assistant/src-tauri/src/main.rs
@@ -1,19 +1,108 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use sysinfo::{
+    Components, Disks, Networks, System,
+    RefreshKind, CpuRefreshKind
+};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SystemData{
+    status: String,
+    total_memory: u64,
+    used_memory: u64,
+    cpu_count: usize,
+    cpu_average_usage: f32,
+    disk_data: Vec<DiskData>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DiskData{
+    disk_id: i32,
+    disk_name: String,
+    disk_mount_point: String,
+    disk_is_removable: bool,
+    disk_total_space: u64,
+    disk_available_space: u64
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 #[tauri::command]
-fn test_invoke(name: &str) -> String {
-    format!("Mister {}", name)
+fn get_system_data() -> String {
+    println!("System Data");
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    println!("=> system:");
+    // RAM and swap information:
+    println!("total memory: {} bytes", sys.total_memory());
+    println!("used memory : {} bytes", sys.used_memory());
+    // Number of CPUs:
+    println!("NB CPUs: {}", sys.cpus().len());
+    
+    sys.refresh_cpu(); // Refreshing CPU information.
+    let mut total_cpu_usage: f32 = 0.0;
+
+    let mut s = System::new_with_specifics(
+        RefreshKind::new().with_cpu(CpuRefreshKind::everything()),
+    );
+
+    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+    // Refresh CPUs again.
+    s.refresh_cpu();
+
+    for cpu in s.cpus() {
+        println!("{}% ", cpu.cpu_usage());
+        total_cpu_usage += cpu.cpu_usage();
+    }
+
+    total_cpu_usage = total_cpu_usage/sys.cpus().len() as f32;
+
+    let mut disk_data_vec: Vec<DiskData> = Vec::new();
+
+    let disks = Disks::new_with_refreshed_list();
+    let mut disk_count = 0;
+    for disk in disks.list(){
+        println!("[{:?}] {:?}", disk.name(), disk.kind());
+        println!("{} {} {} {}", disk.mount_point().display(), disk.total_space(), disk.available_space(), disk.is_removable());
+
+        let disk_data_test = DiskData{
+            disk_id: disk_count,
+            disk_name: String::from(disk.name().to_str().expect("REASON1").to_string()),
+            disk_mount_point: String::from(disk.mount_point().to_str().expect("REASON").to_string()),
+            disk_is_removable: disk.is_removable(),
+            disk_total_space: disk.total_space(),
+            disk_available_space: disk.available_space()
+        };
+        disk_data_vec.push(disk_data_test);
+        disk_count += 1;
+    }
+
+    let response = SystemData{
+        status: String::from("Success!"),
+        total_memory: sys.total_memory(),
+        used_memory: sys.used_memory(),
+        cpu_count: sys.cpus().len(),
+        cpu_average_usage: total_cpu_usage,
+        disk_data: disk_data_vec
+    };
+
+    let json_response = serde_json::to_string(&response).unwrap();
+
+    return json_response;
 }
 
 fn main() {
+
+
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, test_invoke])
+        .invoke_handler(tauri::generate_handler![greet, get_system_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- desktop-assistant/src/components/SystemDataComponent.vue
+++ desktop-assistant/src/components/SystemDataComponent.vue
@@ -1,7 +1,30 @@
 <template>
     <div id="inner-data">
-        <p>System Data</p>
-        <p>{{ greetMsg }}</p>
+        <div id="ram-display">
+            <h3>RAM</h3>
+            <p>{{ ram_used_display }}</p>
+            <p>{{ ram_total_display }}</p>
+        </div>
+
+        <div id="cpu-display">
+            <h3>CPU</h3>
+            <p>{{ cpu_count }}</p>
+            <p>{{ cpu_average_usage }}</p>
+        </div>
+
+        <h3>Storage</h3>
+        <div class="disk-display" v-for="disk in disk_data">
+            <p>{{ disk['disk_name'] }} | {{ disk['disk_mount_point'] }}</p>
+            <p v-if="disk['disk_is_removable']">Removable</p>
+            <p> {{ format_byte(disk['disk_available_space']) }} / {{ format_byte(disk['disk_total_space']) }}</p>
+            <div :style="{'height': '15px', 'width': '100%', 'background-color': 'red'}">
+                <div :style="{'height': '15px', 'width': get_storage_percentage(disk['disk_available_space'],disk['disk_total_space']), 'background-color': 'green'}"></div>
+            </div>
+            <p class="percentage-p">{{ get_storage_percentage(disk['disk_available_space'],disk['disk_total_space']) }}</p>
+
+        </div>
+
+        <button @click="get_system_data">Reload</button>
     </div>
 
 </template>
@@ -13,23 +36,55 @@
         data(){
             return{
                 greetMsg: "",
-                name: "Kay"
+                name: "Kay",
+                ram_used_display: "00GB",
+                ram_total_display: "/00GB RAM",
+                cpu_count: 0,
+                cpu_average_usage: 0,
+                disk_data: [],
+                desk_percentage: ['25%']
             }
         },
         mounted(){
 
             console.log("System Data:")
-            this.name = "lolol"
-            this.greet()
             
-            this.test_invoke("Gary")
+            this.get_system_data()
         },
         methods:{
-            async greet(){
-                this.greetMsg = await invoke("greet", { name: this.name });
+            async get_system_data(){
+                let json = JSON.parse(await invoke("get_system_data"))
+                console.log(json);
+                console.log(json['used_memory']*1e-9);
+
+                this.ram_used_display = `${(json['used_memory']*1e-9).toFixed(2)}GB`;
+                this.ram_total_display = `/${(json['total_memory']*1e-9).toFixed(2)}GB`;
+
+                this.cpu_count = `${json['cpu_count']} cores`;
+                this.cpu_average_usage = `${(json['cpu_average_usage']).toFixed(2)}%`;
+
+                this.disk_data = json['disk_data'];
+                /*
+                for (let i = 0; i < json['disk_data'].length; i++) {
+                    console.log(this.format_byte(json['disk_data'][i]['disk_total_space']))
+                }
+                */
+
             },
-            async test_invoke(input_name){
-                console.log(await invoke("test_invoke", { name: input_name }));
+            format_byte(input_bytes){
+                //console.log(`INPUT: ${input_bytes} | C1: ${1/(1e-6)} | C2: ${1/(1e-9)}`);
+                if(input_bytes < (1/1e-6)){
+                    return `${input_bytes} BYTES`;
+                }if(input_bytes >= (1/1e-6) && input_bytes < (1/1e-9)){
+                    return `${(input_bytes * 1e-6).toFixed(2)} MB`;
+                }else if(input_bytes >= (1/1e-9) && input_bytes < (1/1e-12)){
+                    return `${(input_bytes * 1e-9).toFixed(2)} GB`;
+                }else if(input_bytes >= (1/1e-12)){
+                    return `${(input_bytes * 1e-12).toFixed(2)} TB`;
+                }
+            },
+            get_storage_percentage(current_ammount, max_ammount){
+                return `${((100*current_ammount)/max_ammount).toFixed(2)}%`;
             }
             /*
             async function greet() {
@@ -48,5 +103,33 @@
         margin-right: 15px;
 
         padding-left: 5px;
+    }
+
+    h3{
+        margin: 5px 0px 0px 0px;
+    }
+
+    #ram-display > p{
+        margin: 0;
+    }
+
+    #cpu-display > p{
+        margin: 0;
+    }
+
+    .disk-display{
+        margin-bottom: 15px;
+    }
+    .disk-display > p{
+        margin: 0;
+    }
+
+    .percentage-p{
+        display: block;
+
+        height: 100%;
+        width: 100%;
+
+        margin: 0;
     }
 </style>
